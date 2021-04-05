271928d19898c4d97acc866901884720a730a0aa
Mohammed Bu Saeed <bosaeed2011@gmail.com>
2021-04-05 16:16:29 +0300

    first commit

---
+++ src/main/java/com/alt6wer/demo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.alt6wer.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value = "/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        String name = "Mohammed Ahmed Abobakr Bu Saeed";
+        modelAndView.addObject("name", name);
+        return modelAndView;
+    }
+
+}
